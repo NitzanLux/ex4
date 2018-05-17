30a5c4d3c40c4a0cd7dd2844d584d0d65fe8e0e2
itamar108 <itamar108@gmail.com>
2018-05-17 18:20:46 +0300

    עדכון

---
+++ src/oop/ex4/data_structures/AvlTree.java
@@ -0,0 +1,63 @@
+package oop.ex4.data_structures;
+public class AvlTree {
+
+
+    private AvlNode smallestSonValue;
+    private AvlNode root;
+
+    /**
+     * A default constructor.
+     */
+    public AvlTree()
+    {
+
+    }
+
+
+    /**
+     * A constructor that builds the tree by adding the elements in the input array one-by-one If the same
+     * values appears twice (or more) in the list, it is ignored.
+     * @param data - values to add to tree
+     */
+    public AvlTree(int [] data)
+    {
+
+
+    }
+
+    /**
+     * Add a new node with key newValue into the tree.
+     * @param newValue - new value to add to the tree.
+     * @return - false if newValue already exist in the tree
+     */
+    public boolean add(int newValue)
+    {
+
+    }
+
+
+    /**
+     * Does tree contain a given input value.
+     * @param searchVal - value to search for
+     * @return if val is found in the tree, return the depth of its node (where 0 is the root).
+     * Otherwise -- return -1.
+     */
+    public int contains(int searchVal)
+    {
+
+
+    }
+
+
+    /**
+     * @return the number of nodes in the tree.
+     */
+    public int size()
+    {
+        return this.
+
+    }
+
+
+
+}
