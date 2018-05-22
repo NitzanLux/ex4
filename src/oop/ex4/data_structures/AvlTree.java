d600cde580ef453897e7903421c6f5fc4dc5259c
goolux <nitzanlux@gmail.com>
2018-05-22 23:08:30 +0300

    itamar the hasra on the days we finish the bug i tell you

--- src/oop/ex4/data_structures/AvlTree.java
+++ src/oop/ex4/data_structures/AvlTree.java
@@ -2,14 +2,16 @@ package oop.ex4.data_structures;
 
 import java.util.Iterator;
 
-import static oop.ex4.data_structures.BTreePrinter.printNode;
-
 /**
  * This class is the complete and tested implementation of an AVL-tree.
  *
  * @author itamar108 nlux.
  */
 public class AvlTree extends BinaryTree{
+    public static final int LL = 2;
+    public static final int RL = 1;
+    public static final int RR = -2;
+    public static final int LR = -1;
     int temp=0;//TODO;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     /**
      * A default constructor.
@@ -70,33 +72,56 @@ public class AvlTree extends BinaryTree{
         do {
             balanceTree(currentNode);
             currentNode = currentNode.getParent();
-            System.out.println(temp++);//TODO;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         }while (currentNode!=null);
-
     }
     private void balanceTree(BinaryTreeNode currentNode){
-        int violation= balancedViolation(currentNode);
-        while (violation!=0){
-            if (violation>0) {
-                rotate(currentNode.getLeftChild(), currentNode);
-            }else {
-                rotate(currentNode.getRightChild(),currentNode);
+        int violation=cheackViolation(currentNode);
+        if (Math.abs(violation)<=1){
+            return;
+        }
+        if (violation<0){//right violation
+            BinaryTreeNode rootNode=currentNode.getRightChild();
+            if (cheackViolation(currentNode.getRightChild())==1){
+                rorationToLeft(currentNode.getRightChild(),false);
             }
-            if (Math.abs(violation)==1){
-                rotate(currentNode.getParent(),currentNode.getParent().getParent());
+            rorationToLeft(currentNode,true);
+        }else {
+            if (cheackViolation(currentNode.getLeftChild())==-1){
+                rorationToLeft(currentNode.getLeftChild(),true);
             }
-            currentNode.updateAncestorsDistanceToLeaf();
-            violation= balancedViolation((currentNode));
+            rorationToLeft(currentNode,false);
         }
     }
+    private void rorationToLeft(BinaryTreeNode binaryTreeNode,boolean isLeft){
+        if (isLeft){
+            rotate(binaryTreeNode.getRightChild(),binaryTreeNode);
+        }else {
+            rotate(binaryTreeNode.getLeftChild(),binaryTreeNode);
+        }
+        binaryTreeNode.updateAncestorsDistanceToLeaf();
+    }
+    /*
+    * return violation left-right
+     */
+    private int cheackViolation(BinaryTreeNode currentNode){
+        int leftChildHeight=-1;
+        int rightChildHeight=-1;
+        if (currentNode.getRightChild()!=null){
+            rightChildHeight=currentNode.getRightChild().getBiggestDistanceToLeaf();
+        }
+        if (currentNode.getLeftChild()!=null){
+            leftChildHeight=currentNode.getLeftChild().getBiggestDistanceToLeaf();
+        }
+        return leftChildHeight-rightChildHeight;
+    }
 
-    private void rotate(BinaryTreeNode child, BinaryTreeNode father){
+    private void rotate(BinaryTreeNode child, BinaryTreeNode father){//shouldweRemoveFather
         replaceOnlyChild(father,child);
         if (father==root){
             root=child;
         }
         BinaryTreeNode childOfChildToFather;
-        System.out.println("yes");//TODO tyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+        System.out.println("yes2");//TODO tyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
         if (father.getValue()>child.getValue()){
             childOfChildToFather=child.getRightChild();
         }else {
@@ -104,53 +129,71 @@ public class AvlTree extends BinaryTree{
         }
         father.setChild(childOfChildToFather);
         child.setChild(father);
-        father.updateAncestorsDistanceToLeaf();
     }
-    /*
-     * this function returns the AVL balance factor at a given node. it does so by caluclating the biggest
-     * distance from leaf of the right son, and biggest distance from leaf of the left son, and subtracting
-     * between them.
-     * @param currentNode - the node to return it's balance factor
-     * @return - the AVl balance factor
-     */
-    private int balanceFactor(BinaryTreeNode currentNode){
-        int rightHigte=-1;
-        int leftHight =-1;
-        if (currentNode.getLeftChild()!=null){
-            leftHight=currentNode.getLeftChild().getBiggestDistanceToLeaf();
-        }
-        if (currentNode.getRightChild()!=null){
-            rightHigte=currentNode.getRightChild().getBiggestDistanceToLeaf();
-        }
-        return (leftHight-rightHigte);
-    }
-    /*
-    * defind the kind of violation , 0 no violation , 2 LL , 1 RL ,-1 LR ,-2 RR.
-     */
-    private int balancedViolation(BinaryTreeNode nodeToCheck){//TODO fucking bug i shit you not
-        int violation = balanceFactor(nodeToCheck);
-        if (Math.abs(violation)<=1){
-            return 0;
-        }
-        int leftChildViolation=0;
-        int rightChildViolation=0;
-        if (nodeToCheck.getLeftChild()!=null){
-            leftChildViolation= balanceFactor(nodeToCheck.getLeftChild());}
-        if (nodeToCheck.getRightChild()!=null){
-            rightChildViolation=balanceFactor(nodeToCheck.getRightChild());
-        }
-        if (violation>0){
-            if (leftChildViolation>=0){
-                return 2;
-            }
-            return -1;
-        }else {//TODO megic num of the ahoshloki
-            if (rightChildViolation<=0){
-                return -2;
-            }
-            return 1;
-        }
-    }
+
+
+//    private void balanceTree(BinaryTreeNode currentNode){
+//        int violation= balancedViolation(currentNode);
+//        while (violation!=0){
+//            if (violation<0) {
+//                rotate(currentNode.getRightChild(), currentNode);
+//            }else {
+//                rotate(currentNode.getLeftChild(),currentNode);
+//            }
+//            if (Math.abs(violation)==1){
+//                rotate(currentNode.getParent(),currentNode.getParent().getParent());
+//                currentNode.getParent().getLeftChild().updateAncestorsDistanceToLeaf();
+//                currentNode.getParent().getRightChild().updateAncestorsDistanceToLeaf();
+//            }
+//            currentNode.updateAncestorsDistanceToLeaf();
+//            violation= balancedViolation((currentNode));
+//        }
+//    }
+//    /*
+//     * this function returns the AVL balance factor at a given node. it does so by caluclating the biggest
+//     * distance from leaf of the right son, and biggest distance from leaf of the left son, and subtracting
+//     * between them.
+//     * @param currentNode - the node to return it's balance factor
+//     * @return - the AVl balance factor
+//     */
+//    private int balanceFactor(BinaryTreeNode currentNode){
+//        int rightHigte=-1;
+//        int leftHight =-1;
+//        if (currentNode.getLeftChild()!=null){
+//            leftHight=currentNode.getLeftChild().getBiggestDistanceToLeaf();
+//        }
+//        if (currentNode.getRightChild()!=null){
+//            rightHigte=currentNode.getRightChild().getBiggestDistanceToLeaf();
+//        }
+//        return (leftHight-rightHigte);
+//    }
+//    /*
+//    * defind the kind of violation , 0 no violation , 2 LL , 1 RL ,-1 LR ,-2 LL.
+//     */
+//    private int balancedViolation(BinaryTreeNode nodeToCheck){//TODO fucking bug i shit you not
+//        int violation = balanceFactor(nodeToCheck);
+//        if (Math.abs(violation)<=1){
+//            return 0;
+//        }
+//        int leftChildViolation=0;
+//        int rightChildViolation=0;
+//        if (nodeToCheck.getLeftChild()!=null){
+//            leftChildViolation= balanceFactor(nodeToCheck.getLeftChild());}
+//        if (nodeToCheck.getRightChild()!=null){
+//            rightChildViolation=balanceFactor(nodeToCheck.getRightChild());
+//        }
+//        if (violation<0){
+//            if (leftChildViolation==-1){
+//                return LR;
+//            }
+//            return RR;
+//        }else {//TODO megic num of the ahoshloki
+//            if (rightChildViolation==1){
+//                return RL;
+//            }
+//            return LL;
+//        }
+//    }
 
     /**
      * A method that calculates the minimum numbers of nodes in an AVL tree of height h.
@@ -172,10 +215,7 @@ public class AvlTree extends BinaryTree{
         return (int)(Math.pow(2,h)-1);
     }//TODO added new method
 
-    @Override
-    public Iterator<Integer> iterator() {
-        return super.iterator();
-    }
+
 }
 
 
